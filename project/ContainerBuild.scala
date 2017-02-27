8c88ddc361272f43eb8fadda7b8d4e1ca6029471
Alex Silva <alex-silva@pluralsight.com>
2017-02-27 07:53:29 -0500

    Allowing for a container name (through builder class), removing var from SystemShutdown trait and fixing duplicate ActorSytem creation.

--- project/ContainerBuild.scala
+++ project/ContainerBuild.scala
@@ -23,7 +23,7 @@ object ContainerBuild extends Build {
   lazy val baseSettings = Seq(
     name := "Service Container",
     organization := "com.github.vonnagy",
-    version := "2.0.3",
+    version := "2.0.4",
     description := "Service Container",
     scalaVersion := SCALA_VERSION,
     packageOptions in (Compile, packageBin) +=
